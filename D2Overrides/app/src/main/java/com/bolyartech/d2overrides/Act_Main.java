d82e9fdc96bce7f51373cfc96620f83b5781cdd2
Ognyan Bankov <ogibankov@gmail.com>
2016-03-02 08:57:12 +0200

    initial import

---
+++ D2Overrides/app/src/main/java/com/bolyartech/d2overrides/Act_Main.java
@@ -0,0 +1,14 @@
+package com.bolyartech.d2overrides;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Act_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act__main);
+    }
+}
